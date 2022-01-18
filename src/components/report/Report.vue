4e1058ff6d2b5ff1d0ee73bf6c60ef6e73927e4e
whtpp <939676512@qq.com>
2022-01-18 23:22:57 +0800

    报表完成

---
+++ src/components/report/Report.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 600px; height: 400px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+import _ from 'lodash';
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: "用户来源",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#E9EEF3",
+            },
+          },
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            boundaryGap: false,
+          },
+        ],
+        yAxis: [
+          {
+            type: "value",
+          },
+        ],
+      },
+    };
+  },
+  created() {},
+  async mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status != 200) {
+      return this.$message.error("获取失败");
+    }
+    this.$message.success("获取成功");
+    const result = _.merge(res.data,this.options)
+    myChart.setOption(result);
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from "../components/goods/Params.vue";
 import List from "../components/goods/List.vue";
 import Add from "../components/goods/Add.vue";
 import Order from "../components/order/Order.vue";
+import Report from "../components/report/Report.vue";
 
 Vue.use(Router);
 
@@ -31,7 +32,7 @@ const router = new Router({
         { path: "/Params", component: Params },
         { path: "/goods", component: List },
         { path: "/goods/add", component: Add },
-        { path: "/orders", component: Order },
+        { path: "/reports", component: Report },
       ]
     }
   ]
